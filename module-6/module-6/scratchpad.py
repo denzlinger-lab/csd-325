e5f5b4aaef2db8849ed9eea81cb0187407d1c551
Abram Denzlinger <abram@Abrams-MacBook-Air.local>
2025-11-10 14:34:59 -0600

    11/10/2025 Push

---
+++ module-6/module-6/scratchpad.py
@@ -0,0 +1,151 @@
+"""Forest Fire Sim, modified by Sue Sampson, based on a program by Al Sweigart
+A simulation of wildfires spreading in a forest. Press Ctrl-C to stop.
+Inspired by Nicky Case's Emoji Sim http://ncase.me/simulating/model/
+** use spaces, not indentation to modify **
+Tags: short, bext, simulation"""
+
+import random, sys, time
+
+try:
+    import bext
+except ImportError:
+    print('This program requires the bext module, which you')
+    print('can install by following the instructions at')
+    print('https://pypi.org/project/Bext/')
+    sys.exit()
+
+# Set up the constants:
+WIDTH = 79
+HEIGHT = 22
+
+TREE = 'A'
+FIRE = '@'
+EMPTY = ' '
+
+# adding water symbol
+WATER = '~'
+
+# Defines a lake that occupies the central 20% of the screen width and height
+LAKE_X_START = int(WIDTH * .4)
+LAKE_X_END = int(WIDTH * .6)
+LAKE_Y_START = int(HEIGHT * .4)
+LAKE_Y_END = int(HEIGHT * .6)
+# -----------------------------------------------------------
+
+
+# (!) Try changing these settings to anything between 0.0 and 1.0:
+INITIAL_TREE_DENSITY = 0.20  # Amount of forest that starts with trees.
+GROW_CHANCE = 0.01  # Chance a blank space turns into a tree.
+FIRE_CHANCE = 0.01  # Chance a tree is hit by lightning & burns.
+
+# (!) Try setting the pause length to 1.0 or 0.0:
+PAUSE_LENGTH = 0.5
+
+# New function to check if coordinates are within lake boundary
+def is_in_lake(x, y):
+    return (LAKE_X_START <= x <= LAKE_X_END) and \
+        (LAKE_Y_START <= y <= LAKE_Y_END)
+
+
+def main():
+    forest = createNewForest()
+    bext.clear()
+
+    while True:  # Main program loop
+        displayForest(forest)
+
+        # Run a single simulation step:
+        nextForest = {'width': forest['width'],
+                      'height': forest['height']}
+
+        for x in range(forest['width']):
+            for y in range(forest['height']):
+                if (x, y) in nextForest:
+                    # If we've already set nextForest[(x, y)] on a
+                    # previous iteration, just do nothing here:
+                    continue
+
+                # Gemini called this a 'Runtime Check'
+                # Basically says if the current spot is water, leave it alone
+                if forest[(x, y)] == WATER:
+                    nextForest[(x, y)] = WATER
+                    continue
+                # -----------------------------
+
+                if ((forest[(x, y)] == EMPTY)
+                        and (random.random() <= GROW_CHANCE)):
+                    # Grow a tree in this empty space.
+                    nextForest[(x, y)] = TREE
+                elif ((forest[(x, y)] == TREE)
+                      and (random.random() <= FIRE_CHANCE)):
+                    # Lightning sets this tree on fire.
+                    nextForest[(x, y)] = FIRE
+                elif forest[(x, y)] == FIRE:
+                    # This tree is currently burning.
+                    # Loop through all the neighboring spaces:
+                    for ix in range(-1, 2):
+                        for iy in range(-1, 2):
+                            # Check neighbor coordinates
+                            nx, ny = x + ix, y + iy
+
+                            # Fire spreads to neighboring trees
+                            # Leaves water alone
+                            if (forest.get((nx, ny)) == TREE) and (not is_in_lake(nx, ny)):
+                                nextForest[(nx, ny)] = FIRE
+                    # The tree has burned down now, so erase it:
+                    nextForest[(x, y)] = EMPTY
+                else:
+                    # Just copy the existing object:
+                    nextForest[(x, y)] = forest[(x, y)]
+        forest = nextForest
+
+        time.sleep(PAUSE_LENGTH)
+
+
+def createNewForest():
+    """Returns a dictionary for a new forest data structure."""
+    forest = {'width': WIDTH, 'height': HEIGHT}
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            # Checks position against lake boundary
+            if is_in_lake(x, y):
+                forest[(x, y)] = WATER  # Start as water.
+            # ------------------------------------
+            elif (random.random() * 100) <= INITIAL_TREE_DENSITY:
+                forest[(x, y)] = TREE  # Start as a tree.
+            else:
+                forest[(x, y)] = EMPTY  # Start as an empty space.
+    return forest
+
+
+def displayForest(forest):
+    """Display the forest data structure on the screen."""
+    bext.goto(0, 0)
+    for y in range(forest['height']):
+        for x in range(forest['width']):
+            if forest[(x, y)] == TREE:
+                bext.fg('green')
+                print(TREE, end='')
+            elif forest[(x, y)] == FIRE:
+                bext.fg('red')
+                print(FIRE, end='')
+            # Format water symbol as blue
+            elif forest[(x, y)] == WATER:
+                bext.fg('blue')
+                print(WATER, end='')
+            # -----------------------------
+            elif forest[(x, y)] == EMPTY:
+                print(EMPTY, end='')
+        print()
+    bext.fg('reset')  # Use the default font color.
+    print('Grow chance: {}%  '.format(GROW_CHANCE * 100), end='')
+    print('Lightning chance: {}%  '.format(FIRE_CHANCE * 100), end='')
+    print('Press Ctrl-C to quit.')
+
+
+# If this program was run (instead of imported), run the game:
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()  # When Ctrl-C is pressed, end the program.
